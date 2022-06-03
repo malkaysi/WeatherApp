7eb2f63c42cda956079b5d9d95ba509447689b84
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2022-06-03 13:07:17 -0600

    Eslint

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import { apiid } from './env.js';
 
 let buttonElement = document.getElementById('submitData');
 let showButton = document.getElementById('showData');
+let body = document.getElementsByTagName('body');
 
 let searchObj = {
   cityName,
@@ -21,15 +22,22 @@ function storeSubmissionData() {
   searchObj.zipCode = document.getElementById('zipCode').value;
 }
 
+// Get users current location
 const getGeoLocation = async () => {
   const { cityName, state, zipCode } = searchObj
 
   // Get data if zip inputted
   if (zipCode) {
     try {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${apiid}`)
-    geoLocationData = await response.json();
-    getWeather(geoLocationData);
+      const response = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${apiid}`)
+      geoLocationData = await response.json();
+      console.log(geoLocationData);
+      getWeather(geoLocationData);
+
+      if (geoLocationData.length > 1) {
+        console.log('test')
+      }
+
     } catch {
       console.log(error)
     }
@@ -38,29 +46,33 @@ const getGeoLocation = async () => {
   // Get data if city name and state inputted
   if ((cityName && state) && !zipCode) {
     try {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${state},1&limit=5&appid=${apiid}`)
-    geoLocationData = await response.json();
-    getWeather(geoLocationData);
+      const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${state},1&limit=5&appid=${apiid}`)
+      geoLocationData = await response.json();
+      // getWeather(geoLocationData);
     } catch {
       console.log(error)
+    }
+
+    if (geoLocationData.length > 1) {
+      for (i = 0; i < geoLocationData.length; i++) {
+        displayMultipleLocations(geoLocationData[i]);
+      }
     }
   }
 
   if ((!cityName || !state) && !zipCode) {
     console.log('Please input a city and state or zip code')
   }
-
-  console.log(geoLocationData);
 };
 
+// Get weather info for submitted locations
 const getWeather = async (geoLocationData) => {
   const { lat, lon } = geoLocationData;
-  console.log('test')
 
   if (lat && lon) {
     try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiid}`)
-    weatherData = await response.json();
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiid}`)
+      weatherData = await response.json();
     } catch {
       console.log(error)
     }
@@ -69,9 +81,22 @@ const getWeather = async (geoLocationData) => {
   console.log(weatherData);
 }
 
+// Display results
+// If there are more than one location let's show them radio buttons of the city, country
+const displayMultipleLocations = (location) => {
+  let input = document.createElement("input");
+  input.setAttribute('type', 'radio');
+  input.setAttribute('id', `${location}`);
 
-// Search for the weather of the location
+  let label = document.createElement("label");
+  label.setAttribute('type', 'radio');
+  label.setAttribute('for', `${location}`)
 
+  body.append(input);
+  body.append(label);
+}
+
+// Allow users to submit their data
 buttonElement.addEventListener('click', storeSubmissionData);
 showButton.addEventListener('click', getGeoLocation);
 
